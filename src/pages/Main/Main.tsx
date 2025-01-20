f2cf71bc1395cfa3fe68104c1ffd1a049684bd1e
NanaManBo <kimbosung22@naver.com>
2025-01-20 17:38:16 +0900

    우선 로그인 시 카카오 모달 창 X

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Calendar from '../../components/Calendar/Calendar';
 import Header from '../../components/Header/Header';
 import LoginModal from '../../Modal/LoginModal/LoginModal';
 import { isLoggedIn } from '../../auth/utils/authUtils';
 
 const Main: React.FC = () => {
+  const [loggedIn, setLoggedIn] = useState(false); // 로그인 상태 관리
+
+  useEffect(() => {
+    setLoggedIn(isLoggedIn()); // 컴포넌트 마운트 시 로그인 상태 확인
+  }, []);
+
   return (
     <div>
-      {isLoggedIn() ? <LoginModal /> : <></>}
+      {!loggedIn && <LoginModal />}
       <Header />
       <Calendar />
     </div>
